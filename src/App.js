99ab859f23f89e7fcbec8640d8d552df67ce6ac9
Anton <anton.moroz@helsinki.fi>
2019-05-20 15:28:34 +0300

    PLACEHOLDER AND BACKUP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import loginService from './services/login'
 
-function App() {
+const WelcomeScreen = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Kirjauduit sisään!</div>
+  )
 }
 
-export default App;
+const LoginForm = (props) => {
+  const { handleLoginFunction, email, password, setEmail, setPassword } = props
+  return (
+    <form className="login_form" onSubmit={handleLoginFunction}>
+      <label>Sähköposti</label>
+      <input
+        placeholder="justinar@unity3d.com"
+        type="text"
+        value={email}
+        name="email"
+        onChange={({ target }) => setEmail(target.value)}
+      />
+
+      <label>Salasana</label>
+      <input
+        placeholder="********"
+        type="password"
+        value={password}
+        name="password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+      <button type="submit">kirjaudu</button>
+    </form>
+  )
+}
+
+const App = () => {
+  const [appointments, setAppointments] = useState([])
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  console.log('User currently', user)
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+
+      // Appointmentservice.setToken tms tänne
+      // Appointmentservice.setToken tms tänne
+      // Appointmentservice.setToken tms tänne
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const loggedInUser = await loginService.login({ email, password })
+      window.localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser))
+
+      // Appointmentservice.setToken tms tänne
+      // Appointmentservice.setToken tms tänne
+      // Appointmentservice.setToken tms tänne
+
+      setUser(loggedInUser)
+      setEmail('')
+      setPassword('')
+
+      // console.log('kirjautunut käyttäjä', loggedInUser)
+    } catch (exception) {
+      console.log('virhe kirjautumisessa', exception)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Unity massage booking system</h1>
+      <h2>Kirjautuminen</h2>
+
+      {user === null && <LoginForm handleLoginFunction={handleLogin} email={email} setEmail={setEmail} password={password} setPassword={setPassword} />}
+      {user !== null && <WelcomeScreen />}
+    </div>
+  )
+}
+
+export default App
